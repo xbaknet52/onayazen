00c6a0c1176a14821640f89b6adde582f311ae62
xbaknet52 <141831522+xbaknet52@users.noreply.github.com>
2023-11-03 22:31:13 +0300

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://onayaz.com/en/' + encodeURI(path as string)
+					'https://onayaz.com/' + encodeURI(path as string)
 				}`,
 			},
 		};
